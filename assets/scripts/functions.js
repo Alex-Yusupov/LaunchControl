1e3c2939a2112369daaacd18b5b161e4a9f9e8db
Alex Yusupov <ale2337233@maricopa.edu>
2023-03-24 11:59:21 -0700

    Alexweek8

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -11,3 +11,39 @@ showTime()
                 document.getElementById('gas') .src='./assets/images/gas0.png';
             }
         }  
+        var parked = 0;
+        function park() {
+            if(parked == 0){
+                parked = 1; 
+                document.getElementById('bttn_park') .src='./assets/images/bttn_park_1.PNG';
+                document.getElementById('brake') .src='./assets/images/p1.PNG';
+            } else{
+                parked = 0; 
+                document.getElementById('bttn_park') .src='./assets/images/bttn_park_0.PNG';
+                document.getElementById('brake') .src='./assets/images/p0.PNG';
+            }
+        }  
+        var charge = 0;
+        function crgbatt() {
+            if(charge == 0){
+                charge = 1; 
+                document.getElementById('bttn_batt') .src='./assets/images/bttn_batt_0.PNG';
+                document.getElementById('batt') .src='./assets/images/batt0.PNG';
+            } else{
+                charge = 0; 
+                document.getElementById('bttn_batt') .src='./assets/images/bttn_batt_1.PNG';
+                document.getElementById('batt') .src='./assets/images/batt1.PNG';
+            }
+        }  
+        var trac = 0;
+        function traccont() {
+            if(trac == 0){
+                charge = 1; 
+                document.getElementById('bttn_trac') .src='./assets/images/bttn_trac_1.PNG';
+                document.getElementById('trac') .src='./assets/images/trac0.PNG';
+            } else{
+                trc = 0; 
+                document.getElementById('bttn_trac') .src='./assets/images/bttn_trac_0.PNG';
+                document.getElementById('trac') .src='./assets/images/trac1.PNG';
+            }
+        }  
